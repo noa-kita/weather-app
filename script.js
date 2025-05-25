a13ee6c30b3d9d8a08cf3918ca29fe386808376e
noa.kita <k.noa0130@gmail.com>
2025-05-25 22:04:49 +0900

    feat: 天気APIのデータを取得してアラーム表示する処理を追加

---
+++ script.js
@@ -0,0 +1,25 @@
+
+/* 
+API URL:
+https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key} 
+*/
+const city = 'Tokyo'; // 都市名を指定
+const lat = 35.6895; // 緯度を指定
+const lon = 139.6917; // 経度を指定
+const apiKey = '727acb9ec3b4beeea6dcd7f02781da65';
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=ja`;
+const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},{state code},{country code}&limit={limit}&appid=${apiKey}`;
+
+
+fetch(weatherUrl)
+  .then(response => response.json())  // レスポンスをJSONに変換
+  .then(data => {
+    console.log(data); // 取得データをコンソールで確認
+    const weather = data.weather[0].description;
+    const temp = data.main.temp;
+    console.log(`現在の${city}の天気は「${weather}」、気温は${temp}度です`);
+    alert(`現在の${city}の天気は「${weather}」、気温は${temp}度です`);
+  })
+  .catch(error => {
+    console.error('Error fetching weather data:', error);
+  });
